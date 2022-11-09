1a773d24a51dba9349ccb2eff88dc79ec20943e3
qbentil <bentilshadrack72@gmail.com>
2022-11-09 21:24:04 +0000

    Set up basic functions and methods to implement

--- HashTable/index.js
+++ HashTable/index.js
@@ -1,5 +1,13 @@
+const hastStringToInt = (string, tableSize) => {
+    return 5;
+}
+
 class HashTable{
     setItem = (key, val) => {
         
+    }
+    getItem = (key) => {
+        const index = hastStringToInt(key, this.table?.length);
+        return this.table[index];
     }
 }
