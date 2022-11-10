9970e421694ddb5d0a6030193e6fda9abe69c298
qbentil <bentilshadrack72@gmail.com>
2022-11-10 17:14:02 +0000

    Complete add and get item from hashtyable

--- HashTable/index.js
+++ HashTable/index.js
@@ -1,13 +1,34 @@
 const hastStringToInt = (string, tableSize) => {
-    return 5;
+  let hashkey = 20;
+  for (let i = 0; i < string.length; i++) {
+    hashkey = (hashkey * 31 + string.charCodeAt(i)) % tableSize;
+  }
+
+  return hashkey;
+};
+
+class HashTable {
+  table = new Array(50);
+  setItem = (key, val) => {
+    const idx = hastStringToInt(key, this.table.length);
+    this.table[idx] = { key, val };
+  };
+  getItem = (key) => {
+    const idx = hastStringToInt(key, this.table.length);
+    return this.table[idx];
+  };
 }
 
-class HashTable{
-    setItem = (key, val) => {
-        
-    }
-    getItem = (key) => {
-        const index = hastStringToInt(key, this.table?.length);
-        return this.table[index];
-    }
-}
+
+
+
+// Test
+const myTable = new HashTable();
+myTable.setItem("firstName", "bob");
+myTable.setItem("lastName", "smith");
+myTable.setItem("age", 30);
+myTable.setItem("address", "123 main st");
+myTable.setItem("city", "new york");
+myTable.setItem("state", "ny");
+
+console.log(myTable.getItem("firstName"));
